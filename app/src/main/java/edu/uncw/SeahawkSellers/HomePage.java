32e28bceb74fd6ed5e458f6bf5d83b0ad7501224
Skylar <sf7666@uncw.edu>
2020-03-24 11:46:03 -0400

    Small Change 1

---
+++ app/src/main/java/edu/uncw/SeahawkSellers/HomePage.java
@@ -0,0 +1,15 @@
+package edu.uncw.SeahawkSellers;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+public class HomePage extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+}
