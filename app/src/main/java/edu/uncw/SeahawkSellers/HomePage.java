6d3860b208527d442c83f93a5a5142d713fe91a7
Skylar <sf7666@uncw.edu>
2020-03-25 13:17:08 -0400

    Welcome working and text black

--- app/src/main/java/edu/uncw/SeahawkSellers/HomePage.java
+++ app/src/main/java/edu/uncw/SeahawkSellers/HomePage.java
@@ -5,31 +5,24 @@ import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
-import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
-
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.constraintlayout.widget.ConstraintLayout;
-
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class HomePage extends AppCompatActivity {
     private FirebaseAuth mAuth;
+    private TextView mNameLabel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
-//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-//        setSupportActionBar(toolbar);
         Intent intent = getIntent();
         mAuth = FirebaseAuth.getInstance();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        mNameLabel = findViewById(R.id.hello);
+        mNameLabel.setText(String.format(getResources().getString(R.string.hello), currentUser.getEmail()));
     }
 
         public void signOut(View view) {
